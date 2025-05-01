762e2ef82cd453199396e4fac5625ef14df45239
Арина Нейверт <neyvert@gmail.com>
2025-05-01 16:31:59 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Стивен Кинг',
+        name: 'Оно',
+        publicationYear: 1986,
+        price: 899,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Стивен Кинг',
+        name: 'Сияние',
+        publicationYear: 1977,
+        price: 799,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Стивен Кинг',
+        name: 'Кладбище домашних животных',
+        publicationYear: 1983,
+        price: 749,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Джоан Роулинг',
+        name: 'Гарри Поттер и философский камень',
+        publicationYear: 1997,
+        price: 999,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 1949,
+        price: 599,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Джером Сэлинджер',
+        name: 'Над пропастью во ржи',
+        publicationYear: 1951,
+        price: 649,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Фрэнсис Скотт Фицджеральд',
+        name: 'Великий Гэтсби',
+        publicationYear: 1925,
+        price: 549,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 1967,
+        price: 699,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Анна Каренина',
+        publicationYear: 1877,
+        price: 499,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Фёдор Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 1866,
+        price: 449,
+        bestseller: true,
+        inStock: true
+    }
+];
